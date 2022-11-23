e53ce824f8cf014390bffe0a6dd64a7041f9b9cf
George Burgyan <gburgyan@vec.com>
2022-11-23 12:32:08 -0600

    Rationalize how sub-threads are handled and unify them into the overall model.

--- timing.go
+++ timing.go
@@ -5,15 +5,13 @@ import (
 	"fmt"
 	"sort"
 	"strings"
-	"sync"
 	"time"
 )
 
 type Timing struct {
-	mu             sync.Mutex
-	asyncProcesses map[string]*Timing
-	root           *Location
-	current        *Location
+	root      *Location
+	current   *Location
+	subThread bool
 }
 
 type Location struct {
@@ -21,7 +19,6 @@ type Location struct {
 	EntryCount    int
 	ExitCount     int
 	TotalDuration time.Duration
-	Owner         *Timing
 	Children      map[string]*Location
 }
 
@@ -32,7 +29,7 @@ type key int
 const timingContextKey key = 0
 
 func ContextWithTiming(ctx context.Context) context.Context {
-	t := NewTiming()
+	t := NewTiming("")
 	return context.WithValue(ctx, timingContextKey, t)
 }
 
@@ -47,17 +44,14 @@ func GetTiming(ctx context.Context) *Timing {
 	panic("invalid timing context type")
 }
 
-func NewTiming() *Timing {
-	t := &Timing{
-		asyncProcesses: map[string]*Timing{},
-	}
-
+func NewTiming(rootName string) *Timing {
 	root := &Location{
-		Name:  "", // root
-		Owner: t,
+		Name: rootName, // root
 	}
-	t.current = root
-	t.root = root
+	t := &Timing{
+		current: root,
+		root:    root,
+	}
 
 	return t
 }
@@ -67,35 +61,38 @@ func (t *Timing) Start(name string) Completed {
 		panic("timing name much be a non-zero length string")
 	}
 
-	t.mu.Lock()
 	parent := t.current
 	l := parent.getSubLocation(name)
 	t.current = l
 	l.EntryCount++
-	t.mu.Unlock()
 
 	startTime := time.Now()
 	return func() {
 		d := time.Since(startTime)
 
-		t.mu.Lock()
 		l.ExitCount++
 		l.TotalDuration += d
 		t.current = parent
-		t.mu.Unlock()
 	}
 }
 
-func (t *Timing) BeginAsyncContext(ctx context.Context, name string) context.Context {
-	child := t.BeginAsync(name)
+func (t *Timing) BeginSubThreadContext(ctx context.Context, name string) context.Context {
+	child := t.BeginSubThread(name)
 	return context.WithValue(ctx, timingContextKey, child)
 }
 
-func (t *Timing) BeginAsync(name string) *Timing {
-	child := NewTiming()
-	t.mu.Lock()
-	t.asyncProcesses[name] = child
-	t.mu.Unlock()
+func (t *Timing) BeginSubThread(name string) *Timing {
+	childLoc := t.current.getSubLocation(name)
+	if childLoc.EntryCount > 0 {
+		panic("sub-threads require a new timing location")
+	}
+
+	child := &Timing{
+		current:   childLoc,
+		root:      childLoc,
+		subThread: true,
+	}
+
 	return child
 }
 
@@ -104,39 +101,38 @@ func (l *Location) getSubLocation(name string) *Location {
 		l.Children = map[string]*Location{}
 	}
 
-	if l, ok := l.Children[name]; ok {
-		return l
+	if c, ok := l.Children[name]; ok {
+		return c
 	}
 	c := &Location{
-		Name:  name,
-		Owner: l.Owner,
+		Name: name,
 	}
 	l.Children[name] = c
 	return c
 }
 
 func (t *Timing) String() string {
-	t.mu.Lock()
 	b := strings.Builder{}
-	t.root.dumpToBuilder(&b, "")
-	t.mu.Unlock()
+	t.root.dumpToBuilder(&b, "", "")
 	return b.String()
 }
 
-func (l *Location) dumpToBuilder(b *strings.Builder, prefix string) {
+func (l *Location) dumpToBuilder(b *strings.Builder, prefix, path string) {
 	var childPrefix string
 	if len(l.Name) > 0 {
-		b.WriteString(prefix)
-		b.WriteString(fmt.Sprintf("%s - %s", l.Name, l.TotalDuration.Round(time.Microsecond)))
-		if l.EntryCount != l.ExitCount {
-			b.WriteString(fmt.Sprintf(" entries: %d exits: %d", l.EntryCount, l.ExitCount))
-		} else if l.EntryCount > 1 {
-			b.WriteString(fmt.Sprintf(" calls: %d", l.EntryCount))
+		b.WriteString(fmt.Sprintf("%s%s%s", prefix, path, l.Name))
+		if l.EntryCount > 0 {
+			b.WriteString(fmt.Sprintf(" - %s", l.TotalDuration.Round(time.Microsecond)))
+			if l.EntryCount != l.ExitCount {
+				b.WriteString(fmt.Sprintf(" entries: %d exits: %d", l.EntryCount, l.ExitCount))
+			} else if l.EntryCount > 1 {
+				b.WriteString(fmt.Sprintf(" calls: %d", l.EntryCount))
+			}
 		}
 		b.WriteString("\n")
-		childPrefix = prefix + "." + l.Name
+		childPrefix = path + l.Name + "."
 	} else {
-		childPrefix = prefix
+		childPrefix = path
 	}
 
 	var keys []string
@@ -146,6 +142,6 @@ func (l *Location) dumpToBuilder(b *strings.Builder, prefix string) {
 	sort.Strings(keys)
 	for _, k := range keys {
 		l := l.Children[k]
-		l.dumpToBuilder(b, childPrefix)
+		l.dumpToBuilder(b, prefix, childPrefix)
 	}
 }
